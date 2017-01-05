95417a0ba30ff4790c4f371d70bb835497a50334
AlcholV <robert@mindnect.com>
2017-01-05 15:26:44 +0900

    update : a16 update : overriden log message

---
+++ ACOverrideInjector.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace AlcoholV
+{
+    [StaticConstructorOnStartup]
+    internal static class AcOverrideInjector
+    {
+        static AcOverrideInjector()
+        {
+            LongEventHandler.QueueLongEvent(Inject, "Initializing", true, null);
+        }
+
+        private static Assembly Assembly => Assembly.GetAssembly(typeof (AcOverrideInjector));
+        private static string AssemblyName => Assembly.FullName.Split(',').First();
+
+        private static void Inject()
+        {
+            Log.Message(AssemblyName + " Injecting.");
+            var o = new LoadedOverride();
+            o.OverrideIntoDefs();
+            
+        }
+    }
+}
